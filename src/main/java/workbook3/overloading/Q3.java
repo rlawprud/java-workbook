8662f18bebd0473f64df24228b3da5f9883a41c4
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 12:31:32 +0900

    feat: [overloading] Q3 구현

--- src/main/java/workbook3/overloading/Q3.java
+++ src/main/java/workbook3/overloading/Q3.java
@@ -3,14 +3,22 @@ package main.java.workbook3.overloading;
 import java.util.Scanner;
 
 public class Q3 {
-    // TODO: concat 메서드 오버로딩
+
+    // 문자열을 붙여 return 하는 두 개의 오버로딩 된 메서드 concat 입니다.
+    // 매개변수의 개수에 차이를 둬 오버로딩이 가능합니다.
+    public static String concat(String s1, String s2){
+        return s1 + s2;
+    }
+    public static String concat(String s1, String s2, String s3){
+        return s1+s2+s3;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
         String s3 = sc.next();
-        // TODO: concat 메서드 작성 후 아래 주석 해제
-        // System.out.println(concat(s1,s2));
-        // System.out.println(concat(s1,s2,s3));
+
+        System.out.println(concat(s1,s2));
+        System.out.println(concat(s1,s2,s3));
     }
 }
