67d6b27ef429744ea223b1257fd5e6c4389b707c
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 13:11:05 +0900

    feat: [constructor] Q5 구현

--- src/main/java/workbook3/varinit/Q5.java
+++ src/main/java/workbook3/varinit/Q5.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 class InitPerson {
     static int counter = 0;
     static {
-        // TODO: "class loaded" 출력
+        System.out.println("class loaded");
     }
     String name;
     int id;
     {
-        // TODO: id 부여
+        id = ++counter;
     }
     InitPerson(String name) {
-        // TODO: name 초기화
+        this.name = name;
     }
 }
 
