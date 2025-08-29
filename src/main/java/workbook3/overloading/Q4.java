a784a49c78641e3dfcf7ba18a6f2708796246795
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 12:31:39 +0900

    feat: [overloading] Q4 구현

--- src/main/java/workbook3/overloading/Q4.java
+++ src/main/java/workbook3/overloading/Q4.java
@@ -3,21 +3,38 @@ package main.java.workbook3.overloading;
 import java.util.Scanner;
 
 public class Q4 {
-    // TODO: area 오버로딩 (double r), (double w, double h), (double base, double height)
+
+    public static double area (double r) {
+        return r*r*Math.PI;
+    }
+
+    public static double area (double w, double h){
+        return w*h;
+    }
+
+    // 같은 매개 변수의 타입과 개수를 가진 메서드기 이미 존재하기 때문에,
+    // 메소드 오버로딩이 불가합니다.
+    // 따라서, 메소드의 이름을 바꿉니다.
+    public static double triangleArea (double b, double h) {
+        return b*h/2;
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         String type = sc.next();
+
         if (type.equals("circle")) {
             double r = sc.nextDouble();
-            // System.out.println(area(r)); // TODO: area 메서드 작성 후 주석 해제
+            System.out.println(area(r));
         } else if (type.equals("rect")) {
             double w = sc.nextDouble();
             double h = sc.nextDouble();
-            // System.out.println(area(w,h)); // TODO: area 메서드 작성 후 주석 해제
+            System.out.println(area(w,h));
         } else {
             double b = sc.nextDouble();
             double h = sc.nextDouble();
-            // System.out.println(area(b,h)); // TODO: area 메서드 작성 후 주석 해제
+            System.out.println(triangleArea(b,h));
         }
     }
 }
