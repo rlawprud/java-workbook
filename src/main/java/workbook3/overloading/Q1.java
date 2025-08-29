485d2ac39714fe9e639043948a0b481eb8ece2c7
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 12:31:14 +0900

    feat: [overloading] Q1 구현

--- src/main/java/workbook3/overloading/Q1.java
+++ src/main/java/workbook3/overloading/Q1.java
@@ -3,15 +3,35 @@ package main.java.workbook3.overloading;
 import java.util.Scanner;
 
 public class Q1 {
-    // TODO: printValue 오버로딩 (int), (double), (String)
+
+    // 메서드 오버로딩
+    // 오버로딩은 과다 적재라는 뜻으로, 같은 이름의 메서드를 다른 조건으로 사용 할 때 성립됩니다.
+    // 자세한 메서드 오버로딩 성립 조건은 아래와 같습니다.
+    // 1. 메서드의 이름이 같을 것
+    // 2. 매개변수의 개수 또는 타입이 다를 것
+    // ! 메서드의 매개변수 타입과 개수는 같으나, 리턴 타입이 다를 때는 성립하지 않습니다.
+
+    // 매개변수의 타입이 다른 경우의 오버로딩
+    public static void printValue(int x) {
+        System.out.println(x);
+    }
+    public static void printValue(double x) {
+        System.out.println(x);
+    }
+    public static void printValue(String x) {
+        System.out.println(x);
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int i = sc.nextInt();
         double d = sc.nextDouble();
         String s = sc.next();
-        // TODO: printValue (int), (double), (String) 메서드 작성 후 아래 주석 해제
-        // printValue(i);
-        // printValue(d);
-        // printValue(s);
+
+        printValue(i);
+        printValue(d);
+        printValue(s);
     }
 }
