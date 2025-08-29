9c0cfcb09b84b92ae2dadf338c707470a5d873d3
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 12:31:24 +0900

    feat: [overloading] Q2 구현

--- src/main/java/workbook3/overloading/Q2.java
+++ src/main/java/workbook3/overloading/Q2.java
@@ -3,14 +3,24 @@ package main.java.workbook3.overloading;
 import java.util.Scanner;
 
 public class Q2 {
-    // TODO: max 메서드 오버로딩 (int,int)와 (int,int,int)
+
+    // 매개변수의 개수가 다른 경우의 오버로딩
+    public static int max(int x, int y) {
+        return Math.max(x, y);
+    }
+    public static int max(int x, int y, int z) {
+        return Math.max(x, Math.max(y, z));
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        // TODO: max 메서드 작성 후 아래 주석 해제
-        // System.out.println(max(a,b));
-        // System.out.println(max(a,b,c));
+
+        System.out.println(max(a,b));
+        System.out.println(max(a,b,c));
     }
 }
