722792311da3259798afa7384534b0ee3ef2c7cc
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 13:10:34 +0900

    feat: [constructor] Q2 구현

--- src/main/java/workbook3/varinit/Q2.java
+++ src/main/java/workbook3/varinit/Q2.java
@@ -2,8 +2,12 @@ package main.java.workbook3.varinit;
 
 public class Q2 {
     static int count;
+
+    // static 초기화 블록
+    // static 으로 선언된 필드를 초기화하는 블록입니다.
+    // 클래스가 로딩될 때 한 번 실행됩니다.
     static {
-        // TODO: count 초기화
+        count = 5;
     }
     public static void main(String[] args) {
         System.out.println(count);
