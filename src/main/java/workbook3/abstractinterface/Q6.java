1dd86fe8947748aec303397a5f321d04e5ad5a10
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 18:34:27 +0900

    feat: [abstractinterface] Q6 구현

--- src/main/java/workbook3/abstractinterface/Q6.java
+++ src/main/java/workbook3/abstractinterface/Q6.java
@@ -12,17 +12,17 @@ interface Rotatable {
 
 class Square implements Drawable, Rotatable {
     public void draw() {
-        // TODO: "Square drawn" 출력
+        System.out.println("Square drawn");
     }
 
     public void rotate() {
-        // TODO: "Square rotated" 출력
+        System.out.println("Square rotated");
     }
 }
 
 class Circle implements Drawable {
     public void draw() {
-        // TODO: "Circle drawn" 출력
+        System.out.println("Circle drawn");
     }
 }
 
@@ -35,11 +35,11 @@ public class Q6 {
         Drawable d2 = t2.equals("square") ? new Square() : new Circle();
         d1.draw();
         if (d1 instanceof Rotatable) { // d1이 Rotatable 인터페이스(또는 그 하위 타입)를 실제로 구현한 객체인지 확인
-            // TODO: d1 객체를 활용하여 rotate() 메서드를 호출한다.
+            ((Rotatable) d1).rotate();
         }
         d2.draw();
         if (d2 instanceof Rotatable) { // d2이 Rotatable 인터페이스(또는 그 하위 타입)를 실제로 구현한 객체인지 확인
-            // TODO: d2 객체를 활용하여 rotate() 메서드를 호출한다.
+            ((Rotatable) d2).rotate();
         }
     }
 }
