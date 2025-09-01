0ca227936204bf9e3336f5d8f583185e90f5d3c4
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 17:28:10 +0900

    feat: [thissuper] Q3 구현

--- src/main/java/workbook3/thissuper/Q3.java
+++ src/main/java/workbook3/thissuper/Q3.java
@@ -11,9 +11,9 @@ class Animal {
     }
 }
 
-class Dog /* TODO: Animal 클래스 상속 */ {
+class Dog extends Animal {
     Dog(String name) {
-        // TODO: super() 호출
+        super(name);
         System.out.println("Dog constructor");
     }
 }
@@ -23,7 +23,7 @@ public class Q3 {
         Scanner sc = new Scanner(System.in);
         String n = sc.next();
          Dog dog = new Dog(n);
-         // TODO: Dog 클래스 완성 후 아래 주석 해제
-         // System.out.println(dog.name);
+
+         System.out.println(dog.name);
     }
 }
