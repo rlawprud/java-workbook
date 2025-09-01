d3cd2cff5f983c9d75f095dcef5d27b66b668a5d
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 20:00:19 +0900

    feat: [collections] Q3 구현

--- src/main/java/workbook3/collections/Q3.java
+++ src/main/java/workbook3/collections/Q3.java
@@ -6,13 +6,19 @@ import java.util.HashMap;
 
 public class Q3 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        // TODO: 문자열과 정수 쌍을 N개 입력받아 HashMap<String,Integer>에 저장한 후, 주어진 키에 대한 값을 출력하는 프로그램을 작성하시오.
+        HashMap<String,Integer> map = new HashMap<>();
+
         for(int i=0;i<n;i++) {
             String key = sc.next();
             int val = sc.nextInt();
+            map.put(key,val);
         }
+
         String query = sc.next();
+        System.out.println(map.get(query));
     }
 }
