1ad1eeec7191d46440ca1790c2013ab04374aaae
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 20:00:28 +0900

    feat: [collections] Q4 구현

--- src/main/java/workbook3/collections/Q4.java
+++ src/main/java/workbook3/collections/Q4.java
@@ -6,12 +6,24 @@ import java.util.HashMap;
 
 public class Q4 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         Map<String,Integer> map = new HashMap<>();
+
         for(int i=0;i<n;i++){
             map.put(sc.next(), sc.nextInt());
         }
-        // TODO: map의 키와 값 목록 출력
+
+        for (String key : map.keySet()) {
+            System.out.print(key+" ");
+        }
+
+        System.out.println();
+
+        for (int value : map.values()) {
+            System.out.print(value+" ");
+        }
     }
 }
