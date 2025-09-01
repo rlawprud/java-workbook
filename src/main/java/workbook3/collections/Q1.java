4205aeaf06300f24d70db484811175028fad8eed
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 19:59:52 +0900

    feat: [collections] Q 구현

--- src/main/java/workbook3/collections/Q1.java
+++ src/main/java/workbook3/collections/Q1.java
@@ -6,8 +6,18 @@ import java.util.ArrayList;
 
 public class Q1 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // TODO: 입력받은 문자열을 N개를 ArrayList에 저장한 후 모든 원소를 순서대로 출력하는 프로그램을 작성하시오.
+
+        List<String> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.next());
+        }
+
+        for (String item : arr) {
+            System.out.println(item);
+        }
     }
 }
