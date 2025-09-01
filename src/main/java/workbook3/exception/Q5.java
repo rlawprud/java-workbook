504528cc529668493ada2e27d7b5133c9d6b7ce7
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 19:30:14 +0900

    feat: [exception] Q5 구현

--- src/main/java/workbook3/exception/Q5.java
+++ src/main/java/workbook3/exception/Q5.java
@@ -3,6 +3,9 @@ package main.java.workbook3.exception;
 import java.util.Scanner;
 
 class NegativeAgeException extends Exception {
+    // 상속한 클래스의 생성자를 사용해 메세지를 전달할 수 있도록 설정합니다.
+    // 자바의 예외는 Throwable을 상속합니다.
+    // 따라서, 기본적으로 메세지를 넣어 전달할 수 있습니다.
     NegativeAgeException(String msg) {
         super(msg);
     }
@@ -13,9 +16,11 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
         try {
-            // TODO: age가 0보다 작으면 NegativeAgeException throw
-            throw new NegativeAgeException("Invalid age");
+            if (age < 0) {
+                throw new NegativeAgeException("Invalid age");
+            }
         } catch (NegativeAgeException e) {
+
             System.out.println(e.getMessage());
         }
     }
