dae388d6c246fada5ab78472e5cda818b714d88a
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 20:00:40 +0900

    feat: [collections] Q5 구현

--- src/main/java/workbook3/collections/Q5.java
+++ src/main/java/workbook3/collections/Q5.java
@@ -4,12 +4,24 @@ import java.util.*;
 
 public class Q5 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+
         Set<Integer> set = new HashSet<>();
         for(int i=0;i<n;i++) {
             set.add(sc.nextInt());
         }
-        // TODO: 집합에 저장된 값을 오름차순으로 출력하는 프로그램을 작성하시오. (HashSet과 ArrayList를 조합하여 정렬)
+
+        List<Integer> list = new ArrayList<>(set);
+
+        Collections.sort(list);
+        // 역정렬
+        // Collections.sort(list,Collections.reverseOrder());
+
+        for (int i : list) {
+            System.out.print(i+" ");
+        }
     }
 }
