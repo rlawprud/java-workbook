5777a5504fd9576d583e812a477e902f61ac743d
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 18:33:54 +0900

    feat: [abstractinterface] Q2 구현

--- src/main/java/workbook3/abstractinterface/Q2.java
+++ src/main/java/workbook3/abstractinterface/Q2.java
@@ -8,13 +8,13 @@ interface Playable {
 
 class Piano implements Playable {
     public void play() {
-        // TODO: "Piano plays" 출력
+        System.out.println("Piano plays");
     }
 }
 
 class Guitar implements Playable {
     public void play() {
-        // TODO: "Guitar plays" 출력
+        System.out.println("Guitar plays");
     }
 }
 
