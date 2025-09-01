f83a2fd636532d714120aba0b24bb01703a58212
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-01 17:27:54 +0900

    feat: [thissuper] Q1 구현

--- src/main/java/workbook3/thissuper/Q1.java
+++ src/main/java/workbook3/thissuper/Q1.java
@@ -6,10 +6,11 @@ class Person {
     String name;
     int age;
     Person(String name, int age) {
-        // TODO: this 사용해 초기화
+        this.name = name;
+        this.age = age;
     }
     void printInfo() {
-        // TODO: this를 사용하여 필드 출력
+        System.out.println(this.name + " " + this.age);
     }
 }
 
