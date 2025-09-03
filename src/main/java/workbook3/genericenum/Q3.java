e60208e02acdb56c737567f9fca49da062589712
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-03 19:43:51 +0900

    feat: [genericenum] Q3 구현

--- src/main/java/workbook3/genericenum/Q3.java
+++ src/main/java/workbook3/genericenum/Q3.java
@@ -3,9 +3,15 @@ package main.java.workbook3.genericenum;
 import java.util.Scanner;
 
 public class Q3 {
+    // 상한 타입 파라미터
+    // 제네릭 타입에 특정 상위 클래스나 인터페이스를 제한하여,
+    // 해당 클래스 혹은 인터페이스를 상속 받거나 구현한 클래스여야 한다는 제한입니다.
+    // 이 경우, T는 Number를 상속한 객체여야 합니다.
     static <T extends Number> double sumNumbers(T[] arr) {
-        // TODO: arr 요소의 합을 반환
-        return 0;
+        double sum = 0; // return 값은 double 입니다.
+        for (T number : arr)
+            sum += number.doubleValue(); // 무슨 값이 들어올지 모르니, 최종 값과 동일한 형태로 바꾸어 더해줍니다.
+        return sum;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
