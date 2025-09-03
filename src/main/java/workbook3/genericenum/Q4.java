a0f47741a66a6e0e3e44e98827d415b0228e873c
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-03 19:44:00 +0900

    feat: [genericenum] Q4 구현

--- src/main/java/workbook3/genericenum/Q4.java
+++ src/main/java/workbook3/genericenum/Q4.java
@@ -6,9 +6,18 @@ enum Level { LOW, MEDIUM, HIGH }
 
 public class Q4 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         String input = sc.next();
+
+        // 입력받은 값을 기준으로 enum 에서 동일한 값이 있는지 찾습니다.
+        // 동일한 값이 존재한다면, 해당 값을 할당합니다.
+        // 존재하지 않는 값을 입력했을 시,
+        // IllegalArgumentException : No enum constant
+        // 예외가 발생합니다.
         Level level = Level.valueOf(input);
+
         System.out.println(level);
     }
 }
